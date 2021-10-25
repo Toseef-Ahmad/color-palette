82eef9b27a4b844aa6fd1a59624fcbeb6cc48afb
Toseef-Ahmad <ahmadtouseef946@gmail.com>
2021-10-26 04:58:38 +0500

    finish refector of colorbox css

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -14,6 +14,19 @@ const styles = {
     position: 'relative',
     cursor: 'pointer',
     marginBottom: '-4px',
+    '&:hover button': {
+      opacity: 1,
+      transition: '200ms',
+    },
+  },
+  boxContent: {
+    position: 'absolute',
+    bottom: '0px',
+    left: '0px',
+    width: '100%',
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    fontSize: '12px',
   },
   colorName: {
     color: (props) =>
@@ -54,10 +67,60 @@ const styles = {
     opacity: 0,
     transition: '100ms',
     textDecoration: 'none',
-    '&:hover': {
-      opacity: 1,
-      transition: '200ms',
+  },
+  copyOverly: {
+    opacity: 0,
+    width: '100%',
+    height: '100%',
+    transition: 'transform 0.9s ease-in-out',
+    transform: 'scale(0.1)',
+  },
+  showOverly: {
+    opacity: 1,
+    transform: 'scale(10)',
+    zIndex: 10,
+    position: 'absolute',
+    overflow: 'hidden',
+  },
+  copyMessage: {
+    width: '100%',
+    height: '100%',
+    opacity: 0,
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: 'absolute',
+    padding: 0,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    textAlign: 'center',
+    justifyItems: 'center',
+    '& h1': {
+      fontWeight: 400,
+      textShadow: '1px 2px black',
+      background: 'rgba(255, 255, 255, 0.3)',
+      marginTop: 300,
+      width: '100%',
+      padding: 30,
+      fontSize: 50,
+      textTransform: 'uppercase',
     },
+    '& p': {
+      fontWeight: 100,
+      fontSize: '2rem',
+    },
+  },
+  showMessage: {
+    opacity: 1,
+    color: 'white',
+    position: 'fixed',
+    zIndex: 20,
+    transition: 'all',
+    transitionDuration: '0.4s',
+    transitionTimingFunction: 'ease-in-out',
+    transitionDelay: '0.3s',
   },
 };
 
@@ -72,7 +135,6 @@ const ColorBox = (props) => {
   };
   const { formate, classes } = props;
   const copyOverlay = () => {
-    console.log(props);
     setIsCopyed(true);
     setTimeout(() => {
       setIsCopyed(false);
@@ -84,14 +146,20 @@ const ColorBox = (props) => {
       <CopyToClipboard text={props.color[formate]} onCopy={() => copyOverlay()}>
         <div className={classes.colorBox} style={styles.box}>
           <div
-            className={`ColorBox-overlay ${isCopyed && 'show'}`}
+            className={`${classes.copyOverly} ${
+              isCopyed && classes.showOverly
+            }`}
             style={styles.box}
           ></div>
-          <div className={`ColoBox-copy-text ${isCopyed && 'show'}`}>
+          <div
+            className={`${classes.copyMessage} ${
+              isCopyed && classes.showMessage
+            }`}
+          >
             <h1>COPYED!</h1>
             <p>{props.color[formate]}</p>
           </div>
-          <div className="ColorBox-box-contant">
+          <div className={classes.boxContent}>
             <p className={classes.colorName}>{props.color.name}</p>
           </div>
           <button className={classes.copyButton}>COPY</button>
